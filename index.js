cf3a9c3850d93d2e93131b1db272e85708adfac2
JBXVI <83034974+JBXVI@users.noreply.github.com>
2022-12-18 19:37:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+
 const express = require("express")
 const app = express();
 const port = process.env.PORT || 80
 
 
 app.get('/',(req,res)=>{
-res.send("HELLOOO WELCOME JIBIN")
+    res.send("HELLOOO WELCOME JIBIN")
 })
 
 app.get('/pp',(req,res)=>{
-  res.send("hellooooo pal")
+    res.send("hellooooo pal")
 })
-app.listen(port, ()=>{console.log("JBXVserver running on port 80")}
+app.listen(port, ()=>{console.log("JBXVserver running on port 80")})
